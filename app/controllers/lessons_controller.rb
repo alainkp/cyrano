2557f820f9a72f9689045e813353868f3bf1b560
Basile Quinchon <basile.quinchon@gmail.com>
2019-11-25 17:13:36 +0100

    poem show /create lesson

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,2 +1,17 @@
 class LessonsController < ApplicationController
+  skip_before_action :authenticate_user!
+
+  def show
+    @lesson = Lesson.find(params[:id])
+  end
+
+  def create
+    @lesson = Lesson.new
+    @poem = Poem.find(params[:poem_id])
+    @user = current_user
+    @lesson.user = @user
+    @lesson.poem = @poem
+    @lesson.save
+    redirect_to lesson_path(@lesson)
+  end
 end
--- app/controllers/poems_controller.rb
+++ app/controllers/poems_controller.rb
@@ -1,5 +1,8 @@
 class PoemsController < ApplicationController
+  skip_before_action :authenticate_user!
+
   def show
     @poem = Poem.find(params[:id])
+    @lesson = Lesson.new
   end
 end
