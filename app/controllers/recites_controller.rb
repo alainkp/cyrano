6fde9364805203cd73d450d461251cbb2c5f962d
Alain KONG <kong.alain@gmail.com>
2019-11-25 18:03:02 +0100

    recite

--- app/controllers/recites_controller.rb
+++ app/controllers/recites_controller.rb
@@ -1,2 +1,18 @@
 class RecitesController < ApplicationController
+  def show
+    @recite = Recite.find(params[:id])
+  end
+
+  def create
+    @lesson = Lesson.find(params[:lesson_id])
+    @recite = Recite.new()
+    @recite.lesson = @lesson
+    @recite.save
+    redirect_to recite_path(@recite)
+  end
+
+  private
+
+  def recite_params
+  end
 end
