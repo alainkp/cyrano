00c3b25be4db88f3766836046d01a748611a3f18
Alain KONG <kong.alain@gmail.com>
2019-11-25 16:51:36 +0100

    seed with complete data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,34 @@
 require 'open-uri'
 require 'nokogiri'
-Poem.destroy_all
+
+USERS = [
+  {
+    email: 'basile@cyrona.com',
+    password: 123456,
+    first_name: 'Basile',
+    last_name: 'QUINCHON'
+  },
+  {
+    email: 'medhi@cyrona.com',
+    password: 123456,
+    first_name: 'Mehdi',
+    last_name: 'SIJELMASSI'
+  },
+  {
+    email: 'antoine@cyrona.com',
+    password: 123456,
+    first_name: 'Antoine',
+    last_name: 'FILLER'
+  },
+  {
+    email: 'alain@cyrona.com',
+    password: 123456,
+    first_name: 'Alain',
+    last_name: 'KONG'
+  }
+]
+
+
 def scrap_poems
   url = 'https://wheatoncollege.edu/vive-voix/par-incipit/'
   list = Nokogiri::HTML(open(url).read).search('.columns.small-12.main li a')
@@ -28,4 +56,64 @@ def scrap_poems
     puts "Created #{poem.title} by #{poem.author_name}"
   end
 end
-scrap_poems
+
+def seed_users
+  USERS.each_with_index do |user_hash, i|
+    new_user = User.new(user_hash)
+    new_user.save!
+    puts "##{i+1} #{new_user.email} is create."
+  end
+end
+
+def seed_lessons
+  User.all.each_with_index do |user, i|
+    puts "Create lessons for #{user.last_name} #{user.first_name}."
+    # Create lessons with random progression
+    3.times do
+      lesson = Lesson.new(user: user, poem: Poem.all.sample)
+      lesson.reading_progression = rand(1..100)
+      lesson.listening_progression = rand(1..100)
+      recite = Recite.new(progression: rand(1..100), duration: rand(1000..30000))
+      recite.lesson = lesson
+      recite.save!
+      lesson.save!
+      puts "Lesson #{lesson.id} with random progression created."
+    end
+
+    # Create lessons with 0% progression
+    3.times do
+      lesson = Lesson.new(user: user, poem: Poem.all.sample)
+      lesson.reading_progression = 0
+      lesson.listening_progression = 0
+      recite = Recite.new(progression: 0, duration: 0)
+      recite.lesson = lesson
+      recite.save!
+      lesson.save!
+      puts "Lesson #{lesson.id} with 0\% progression created."
+    end
+  end
+end
+
+
+if Poem.count == 0
+  puts "⚠️  Destroy all poems"
+  Poem.destroy_all
+  puts "➕  Seed poems..."
+  scrap_poems
+else
+  puts "⚠️  No poems inserted, you already have #{Poem.count} !"
+end
+
+puts "⚠️  Destroy all recites"
+Recite.destroy_all
+puts "⚠️  Destroy all lessons"
+Lesson.destroy_all
+puts "⚠️  Destroy all users"
+User.destroy_all
+
+puts "➕  Seed users..."
+seed_users
+
+puts "➕  Seed lessons..."
+seed_lessons
+
