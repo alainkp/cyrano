77491e07edfdf25ea2528c0d51284656a8241bf6
Antoine Fillet <momentumcorporatefinance@gmail.com>
2019-11-25 17:05:29 +0100

    index

--- app/controllers/poems_controller.rb
+++ app/controllers/poems_controller.rb
@@ -1,5 +1,10 @@
 class PoemsController < ApplicationController
+  skip_before_action :authenticate_user!
   def show
     @poem = Poem.find(params[:id])
+  end
+
+  def index
+    @poems = Poem.all
   end
 end
