166418337b37c3f9fb6c2611af7dde03cff4f015
Basile Quinchon <basile.quinchon@gmail.com>
2019-11-25 18:03:32 +0100

    components page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,13 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [:home]
+  skip_before_action :authenticate_user!, only: [:home, :components]
 
   def home
+  end
+
+  def components
+    @lesson = Lesson.first
+    @user = User.first
+    @poem = Poem.first
+    @recite = Recite.first
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
+  get '/components', to: 'pages#components'
   get '/dashboard', to: 'users#dashboard'
 
   resources :poems, only: [:index, :show] do
