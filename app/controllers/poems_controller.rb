64c3b4ae28fa3a414e82c49b31a1aa07672f2437
Antoine Fillet <momentumcorporatefinance@gmail.com>
2019-11-25 18:27:29 +0100

    searchB

--- app/controllers/poems_controller.rb
+++ app/controllers/poems_controller.rb
@@ -1,12 +1,21 @@
 class PoemsController < ApplicationController
   skip_before_action :authenticate_user!
-  
-  def index
-    @poems = Poem.all
-  end
-  
+
+  # def index
+  # @poems = Poem.all
+  # end
+
   def show
     @poem = Poem.find(params[:id])
     @lesson = Lesson.new
-  end  
+  end
+
+  def index
+    if params[:query].present?
+      sql_query = "title ILIKE :query OR author_name ILIKE :query"
+      @poems = Poem.where(sql_query, query: "%#{params[:query]}%")
+    else
+      @poems = Poem.all
+    end
+  end
 end
