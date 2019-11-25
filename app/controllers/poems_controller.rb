c412613af8197b09209e29d293171098a8557a2e
Basile Quinchon <basile.quinchon@gmail.com>
2019-11-25 17:42:43 +0100

    links in lesson show

--- app/controllers/poems_controller.rb
+++ app/controllers/poems_controller.rb
@@ -1,12 +1,12 @@
 class PoemsController < ApplicationController
   skip_before_action :authenticate_user!
-  
+
   def index
     @poems = Poem.all
   end
-  
+
   def show
     @poem = Poem.find(params[:id])
     @lesson = Lesson.new
-  end  
+  end
 end
