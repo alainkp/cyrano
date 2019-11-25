e7c5328d79703d14110537f3a69f806ea6790c88
Basile Quinchon <basile.quinchon@gmail.com>
2019-11-25 15:46:48 +0100

    seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,31 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'open-uri'
+require 'nokogiri'
+Poem.destroy_all
+def scrap_poems
+  url = 'https://wheatoncollege.edu/vive-voix/par-incipit/'
+  list = Nokogiri::HTML(open(url).read).search('.columns.small-12.main li a')
+  list.each_with_index do |poem, i|
+    next if i.odd?
+    poem_url = poem.attributes['href'].value.to_s
+    author = list[i + 1].text.strip
+    if !poem_url.match?(/(https:)/)
+      content = Nokogiri::HTML(open("https://wheatoncollege.edu#{poem_url}").read).search('.columns.small-12.main p').text.strip.split('dit par').first
+      title = Nokogiri::HTML(open("https://wheatoncollege.edu#{poem_url}").read).search('.hero__title').text.strip
+      audio = Nokogiri::HTML(open("https://wheatoncollege.edu#{poem_url}").read).search('.wp-audio-shortcode a').text.strip
+    else
+      content = Nokogiri::HTML(open(poem_url).read).search('.columns.small-12.main p').text.strip.split('dit par').first
+      title = Nokogiri::HTML(open(poem_url).read).search('.hero__title').text.strip
+      audio = Nokogiri::HTML(open(poem_url).read).search('.wp-audio-shortcode a').text.strip
+    end
+    poem = Poem.new(
+      title: title,
+      url: poem_url,
+      author_name: author,
+      content: content,
+      audio: audio
+    )
+    poem.save!
+    puts "Created #{poem.title} by #{poem.author_name}"
+  end
+end
+scrap_poems
