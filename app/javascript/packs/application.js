0f3ca08b37c20fa7ade10d3897f27a80065d5387
Alain KONG <kong.alain@gmail.com>
2019-11-26 15:32:12 +0100

    progress bar js

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,4 +1,6 @@
 import "bootstrap";
 import { initAudio } from "../components/audio_timecode.js";
+// import { initAudioProgress } from "../components/audio_progress_bar.js";
 
 initAudio();
+// initAudioProgress();
