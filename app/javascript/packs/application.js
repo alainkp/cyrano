80fd415a683fa8b1d251eef08b8c59ad3b361757
Alain KONG <kong.alain@gmail.com>
2019-11-26 18:15:52 +0100

    lesson show

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,11 +2,14 @@ import "bootstrap";
 import ProgressBar from "progressbar.js";
 import { speechToText } from "../components/speech_to_text.js";
 import { initAudio } from "../components/audio_timecode.js";
-import { lessonProgressBar } from "../components/lessonProgressBar";
+import { lessonProgressBar, lessonShowProgression } from "../components/lessonProgressBar";
 
 speechToText();
 initAudio();
 
 if (document.querySelector("#lessons-card")) {
   lessonProgressBar();
+}
+if (document.querySelector(".lesson-show-progression")) {
+  lessonShowProgression();
 }
