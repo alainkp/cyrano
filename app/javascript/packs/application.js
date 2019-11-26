1cd47e36d83e2359b094a415f0fd7c8a53219a65
Basile Quinchon <basile.quinchon@gmail.com>
2019-11-26 18:06:04 +0100

    init voice reco

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,9 +1,9 @@
 import "bootstrap";
 import {init_audio_timecode } from "../components/audio_timecode.js";
 import { speechToText } from "../components/speech_to_text.js";
-// import { recordSpeech } from "../components/record_speech.js";
+import { createPoemArray } from "../components/poem_to_array.js";
 
 init_audio_timecode();
 speechToText();
-// recordSpeech();
+createPoemArray();
 
