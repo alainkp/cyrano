74bc9a7461e764bf8f95eac3fcaa8f3558fd6033
Alain KONG <kong.alain@gmail.com>
2019-11-26 12:28:08 +0100

    add continue lesson

--- app/javascript/components/audio_timecode.js
+++ app/javascript/components/audio_timecode.js
@@ -3,9 +3,6 @@ const audiSetTimecode = () => {
   if (audio) {
     const input_audio_progr = document.getElementById('lesson_listening_progression');
     const p = document.querySelector('.progress-bar');
-    console.log(audio.duration);
-    console.log(input_audio_progr.value);
-    console.log(audio.duration * input_audio_progr.value / 100);
     audio.currentTime = audio.duration * input_audio_progr.value / 100;
     p.style.width = input_audio_progr.value;
     audio.addEventListener('timeupdate',() => {
