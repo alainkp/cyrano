91c892a08d7a9cc9d20cd7447dce1fae91fd63df
Basile Quinchon <basile.quinchon@gmail.com>
2019-11-27 18:28:34 +0100

    poem debug

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,7 +3,7 @@ import ProgressBar from "progressbar.js";
 import { speechToText,initRecordButton } from "../components/speech_to_text.js";
 import { initAudio } from "../components/audio_timecode.js";
 import { lessonProgressBar, lessonShowProgression, lessonOverallProgress } from "../components/lessonProgressBar";
-import { loadDynamicDashboardText } from '../components/banner';
+// import { loadDynamicDashboardText } from '../components/banner';
 
 initAudio();
 
