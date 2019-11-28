b623f8a55aff27b3cdee047f18deace10b5ee3ee
Basile Quinchon <basile.quinchon@gmail.com>
2019-11-28 19:10:25 +0100

    sample image on dashboard

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,5 +2,6 @@ class UsersController < ApplicationController
   def dashboard
     @lessons = current_user.lessons
     @recites = current_user.recites
+    @index_image = ['fennec', 'bee', 'fox', 'fox2', 'cat', 'bee2', 'fly', 'cocci', 'kid', 'rena', 'bug', 'scar', 'snowm', 'kids']
   end
 end
