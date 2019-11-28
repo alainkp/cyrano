35bc2d2b537bcb54365487f61ee483bf26399a1c
Basile Quinchon <basile.quinchon@gmail.com>
2019-11-28 22:00:44 +0100

    added extension to index-images

--- app/controllers/poems_controller.rb
+++ app/controllers/poems_controller.rb
@@ -2,7 +2,7 @@ class PoemsController < ApplicationController
   skip_before_action :authenticate_user!
 
   def index
-    @index_image = ['fennec', 'bee', 'fox', 'fox2', 'cat', 'bee2', 'fly', 'cocci', 'kid', 'rena', 'bug', 'scar', 'snowm', 'kids']
+    @index_image = ['fennec.svg', 'bee.svg', 'fox.svg', 'fox2.svg', 'cat.svg', 'bee2.svg', 'fly.png', 'cocci.svg', 'kid.svg', 'rena.svg', 'bug.png', 'scar.png', 'snowm.png', 'kids.png']
     if params[:query].present?
       sql_query = "title ILIKE :query OR author_name ILIKE :query"
       @poems = Poem.where(sql_query, query: "%#{params[:query]}%")
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,6 @@ class UsersController < ApplicationController
   def dashboard
     @lessons = current_user.lessons
     @recites = current_user.recites
-    @index_image = ['fennec', 'bee', 'fox', 'fox2', 'cat', 'bee2', 'fly', 'cocci', 'kid', 'rena', 'bug', 'scar', 'snowm', 'kids']
+    @index_image = ['fennec.svg', 'bee.svg', 'fox.svg', 'fox2.svg', 'cat.svg', 'bee2.svg', 'fly.png', 'cocci.svg', 'kid.svg', 'rena.svg', 'bug.png', 'scar.png', 'snowm.png', 'kids.png']
   end
 end
