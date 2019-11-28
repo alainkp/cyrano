cd4d02382db302e564db05a42cdeee41e28b5e5e
Alain KONG <kong.alain@gmail.com>
2019-11-28 15:19:17 +0100

    add % on reading

--- app/javascript/components/tap_to_read.js
+++ app/javascript/components/tap_to_read.js
@@ -1,4 +1,5 @@
 import ProgressBar from "progressbar.js";
+
 const linearGradient = `
 <defs>
   <linearGradient id="gradient" x1="0%" y1="0%" x2="100%" y2="0%" gradientUnits="userSpaceOnUse">
@@ -7,6 +8,7 @@ const linearGradient = `
     <stop offset="100%" stop-color="#96c93d"/>
   </linearGradient>
 </defs>`;
+
 const initReadingProgressBar = () => {
   // let bar = {}
   const readingProgressBar = document.getElementById('reading-progress-bar');
@@ -40,60 +42,32 @@ const initReadingProgressBar = () => {
 
     bar.text.style.fontFamily = '"Raleway", Helvetica, sans-serif';
     bar.text.style.fontSize = '2rem';
+    // bar.animate(0.4);
     bar.svg.insertAdjacentHTML('afterBegin', linearGradient);
     // Number from 0.0 to 1.0
     return bar
   }
 }
 
-// const audioSetTimecode = () => {
-
-//   const audio = document.getElementById('listening-audio');
-//   const readingProgressBar = document.getElementById('reading-progress-bar');
-//   const bar = initReadingProgressBar();
-//   if (audio) {
-//     console.log('toto');
-//     const input_audio_progr = document.getElementById('lesson_listening_progression');
-//     const p = document.querySelector('.progress-bar');
-//     audio.currentTime = audio.duration * input_audio_progr.value / 100;
-//     p.style.width = input_audio_progr.value;
-//     audio.addEventListener('timeupdate',() => {
-//       input_audio_progr.value = Math.ceil(audio.currentTime / audio.duration * 100);
-//       // console.log(input_audio_progr.value);
-//       p.style.width = `${input_audio_progr.value}%`
-//       bar.animate(input_audio_progr.value/100);
-//     });
-//   }
-// }
-
 const readingProgressBar = () => {
-  // const readingProgressBar = document.getElementById('reading-progress-bar');
-  const bar = initReadingProgressBar();
-  const p = document.querySelector('.reading-progress-bar')
-  if (p) {
-    let overallProgression = 0;
-    const poemOverallLength = document.querySelectorAll('.poem-reading-content p').length;
-    console.log(poemOverallLength)
-    let unreadLinesLength = document.querySelectorAll('.poem-reading-line-hidden').length;
-    console.log(unreadLinesLength)
-    let progression = 100 - (unreadLinesLength * 100 / poemOverallLength);
-    console.log(progression)
-    overallProgression += Number.parseInt(progression,10);
-    console.log(overallProgression)
-    p.style.width = `${overallProgression}%`;
-    bar.animate(overallProgression/100);
-  }
+  let overallProgression = 0;
+  const poemOverallLength = document.querySelectorAll('.poem-reading-content p').length;
+  let unreadLinesLength = document.querySelectorAll('.poem-reading-line-hidden').length;
+  let progression = 100 - (unreadLinesLength * 100 / poemOverallLength);
+  overallProgression += Number.parseInt(progression,10);
+  return overallProgression/100
 }
 
 const tapToRead = () => {
   let scrolling = 0;
-  readingProgressBar();
+  const bar = initReadingProgressBar();
   const poemContent = document.querySelector('.poem-reading-content');
   document.addEventListener('touchstart', (event) => {
     if (document.querySelector('.start-container.hidden')) {
       if (document.querySelector('.poem-reading-line-hidden')) {
         const shownLine = document.querySelector('.poem-reading-line');
         const hiddenLine = document.querySelector('.poem-reading-line-hidden');
+        const readingProgression = document.querySelector('#lesson_reading_progression');
         let elementHeight = document.querySelector('.poem-reading-line').offsetHeight - 20;
         shownLine.classList.remove('poem-reading-line');
         shownLine.classList.add('poem-reading-line-viewed');
@@ -101,6 +75,8 @@ const tapToRead = () => {
         hiddenLine.classList.add('poem-reading-line');
         scrolling += elementHeight;
         poemContent.scroll(0, scrolling);
+        readingProgression.value = readingProgressBar()*100;
+        bar.animate(readingProgressBar());
       }
     }
   }, false);
