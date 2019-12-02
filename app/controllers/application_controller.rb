707d44a40f3c02ffc10a0d8c506fc55a9eafb205
mehdi sijelmassi <mehdisijelmassi1@gmail.com>
2019-12-02 11:14:28 +0100

    Open Graph meta tags are properly set

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,9 @@
 class ApplicationController < ActionController::Base
-  
+
   before_action :authenticate_user!
+
+  def default_url_options
+    { host: ENV["www.cyrano.fun"] || "localhost:3000" }
+  end
+
 end
